de7d1aca3d1487242cbe80e123055d971399400f
Adriana <108908409+adrimdip@users.noreply.github.com>
2023-05-22 19:03:33 -0300

    Update validar.js

--- validar.js
+++ validar.js
@@ -49,23 +49,5 @@ function validarFormulario() {
   }
   return true
 
- const $form = document.querySelector('#contactform');
-$form.addEventListener("submit", handleSubmit);
-
-async function handleSubmit(event) {
-  event.preventDefault();
-  const form = new FormData(this);
-  const response = await fetch(this.action, {
-    method: this.method,
-    body: form,
-    headers: {
-      Accept: "application/json",
-    },
-  });
-
-  if (response.ok) {
-    this.reset();
-    alert("Gracias por contactarnos. Te escribiremos a la brevedad.");
-  }
-}
+ 
 
